70b5e9ceb494cc571045f8a4dbb118fc80ad24b8
Matthieu Gautier <mgautier@kymeria.fr>
2018-07-17 15:57:20 +0200

    Set the icon of the application

--- kiwixapp.cpp
+++ kiwixapp.cpp
@@ -4,6 +4,10 @@
 KiwixApp::KiwixApp(int& argc, char *argv[])
     : QApplication(argc, argv)
 {
+    auto icon = QIcon();
+    icon.addFile(":/icons/kiwix/app_icon.svg");
+    setWindowIcon(icon);
+
     mainWindow = new MainWindow;
     setApplicationName("kiwix-desktop");
     mainWindow->show();
