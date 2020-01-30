c804cc5d3bdefe4ee46af0db06dac28d7704151c
Matthieu Gautier <mgautier@kymeria.fr>
2020-01-30 15:59:17 +0100

    Move the list of all languages in another file.

---
+++ src/static_content.h
@@ -0,0 +1,12 @@
+
+#ifndef STATICCONTENT_H
+#define STATICCONTENT_H
+
+#include <QLocale>
+#include <vector>
+
+extern std::vector<QLocale::Language> S_LANGUAGES;
+
+void initStaticContent();
+
+#endif //STATICCONTENT_H
