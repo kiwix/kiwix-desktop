0f24892294a54be79741deae78464e9541d309f3
Nikhil Tanwar <2002nikhiltanwar@gmail.com>
2023-07-16 23:10:17 +0530

    New settings entry: Move files to trash
    Added a new settings entry to toggle if deleted files should move to trash/recycle bin.

--- src/settingsview.h
+++ src/settingsview.h
@@ -11,16 +11,18 @@ class SettingsView : public QWidget
 public:
     SettingsView(QWidget *parent = nullptr);
     ~SettingsView(){};
-    void init(int zoomPercent, const QString &downloadDir, const QString &monitorDir);
+    void init(int zoomPercent, const QString &downloadDir, const QString &monitorDir, const bool moveToTrash);
 public Q_SLOTS:
     void resetDownloadDir();
     void browseDownloadDir();
     void browseMonitorDir();
     void clearMonitorDir();
     void setZoom(int zoomPercent);
+    void setMoveToTrash(bool moveToTrash);
     void onDownloadDirChanged(const QString &dir);
     void onMonitorDirChanged(const QString &dir);
     void onZoomChanged(qreal zoomFactor);
+    void onMoveToTrashChanged(bool moveToTrash);
 private:
     bool confirmDialogDownloadDir(const QString& dir);
     bool confirmDialog(QString messageText, QString messageTitle);
