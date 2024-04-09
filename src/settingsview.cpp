ccb13239518ad9a7c6390dd2fe55df740f37751d
sgourdas <jim31700@gmail.com>
2024-04-09 19:00:59 +0300

    Compilation error fix from clarity changes

--- src/settingsview.cpp
+++ src/settingsview.cpp
@@ -35,7 +35,7 @@ SettingsView::SettingsView(QWidget *parent)
     ui->moveToTrashLabel->setText(gt("move-files-to-trash"));
     ui->reopenTabLabel->setText(gt("open-previous-tabs-at-startup"));
 #if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
-    ui->line_5->hide();
+    ui->lineMonitor->hide();
     ui->moveToTrashLabel->hide();
     ui->moveToTrashToggle->hide();
 #endif
