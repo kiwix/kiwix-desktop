7d7d238a47101f3f5093204d5b440a07d3a0d4e3
ShaopengLin <shaopeng.lin@mail.utoronto.ca>
2024-09-26 02:05:55 -0400

    Add Hide Button in TOC
    Extra button for user to close TOC

--- src/kiwixwebchannelobject.cpp
+++ src/kiwixwebchannelobject.cpp
@@ -6,7 +6,18 @@ QString KiwixWebChannelObject::getTocTitle() const
     return gt("table-of-content");
 }
 
+QString KiwixWebChannelObject::getHideButtonText() const
+{
+    return gt("hide");
+}
+
 bool KiwixWebChannelObject::getTocVisible() const
 {
     return KiwixApp::instance()->getAction(KiwixApp::ToggleTOCAction)->isChecked();
+}
+
+void KiwixWebChannelObject::setTocVisible(bool visible)
+{
+    if (getTocVisible() != visible)
+        KiwixApp::instance()->getAction(KiwixApp::ToggleTOCAction)->toggle();
 }
