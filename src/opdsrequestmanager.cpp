7327fa45dd1411f4d340c343b683da0951e371b1
praveendeepak_ <koripallipraveendeepak@gmail.com>
2025-12-03 19:46:26 +0530

    Update OPDS host to opds.library.kiwix.org and enable redirects
    - Switch default host to `opds.library.kiwix.org`
    - Update API paths to remove `/catalog` prefix
    - Enable `RedirectPolicyAttribute` to follow HTTP redirects
    - Add class documentation
    
    Fixes #1374

--- src/opdsrequestmanager.cpp
+++ src/opdsrequestmanager.cpp
@@ -1,10 +1,5 @@
 #include "opdsrequestmanager.h"
 #include "kiwixapp.h"
-#include <QPointer>
-
-namespace {
-constexpr int kMaxRedirects = 5;
-}
 
 OpdsRequestManager::OpdsRequestManager()
 {
@@ -15,7 +10,7 @@ QString OpdsRequestManager::getCatalogHost()
     const char* const envVarVal = getenv("KIWIX_CATALOG_HOST");
     return envVarVal
          ? envVarVal
-         : "library.kiwix.org";
+         : "opds.library.kiwix.org";
 }
 
 int OpdsRequestManager::getCatalogPort()
@@ -24,44 +19,6 @@ int OpdsRequestManager::getCatalogPort()
     return envVarVal
          ? atoi(envVarVal)
          : 443;
-}
-
-// Helper to handle replies and follow redirects
-void OpdsRequestManager::handleReply(QNetworkReply* reply, std::function<void(QNetworkReply*)> finalHandler, int redirectCount) {
-    QVariant redirectAttr = reply->attribute(QNetworkRequest::RedirectionTargetAttribute);
-    if (redirectAttr.isValid() && redirectCount < kMaxRedirects) {
-        QUrl redirectUrl = redirectAttr.toUrl();
-        // Make absolute if needed
-        if (redirectUrl.isRelative()) {
-            redirectUrl = reply->url().resolved(redirectUrl);
-        }
-        qInfo() << "Following redirect" << redirectCount + 1 << "to:" << redirectUrl.toString();
-        reply->deleteLater();
-        QNetworkRequest newRequest(redirectUrl);
-        QNetworkReply* newReply = m_networkManager.get(newRequest);
-        connect(newReply, &QNetworkReply::finished, this, [=]() {
-            handleReply(newReply, finalHandler, redirectCount + 1);
-        });
-        return;
-    }
-    // No redirect, or max redirects reached
-    if (redirectAttr.isValid() && redirectCount >= kMaxRedirects) {
-        qWarning() << "Redirect limit (" << kMaxRedirects << ") reached. Reporting error.";
-        // Set a custom property to indicate redirect limit reached
-        reply->setProperty("redirectLimitReached", true);
-        // Optionally, abort the reply to set an error
-        reply->abort();
-    }
-    if (redirectCount > 0) {
-        qInfo() << "Completed after" << redirectCount << "redirects";
-    }
-    finalHandler(reply);
-}
-
-// New method to allow requests to arbitrary URLs (for redirects)
-QNetworkReply* OpdsRequestManager::opdsResponseFromUrl(const QUrl &url) {
-    QNetworkRequest request(url);
-    return m_networkManager.get(request);
 }
 
 void OpdsRequestManager::doUpdate(const QString& currentLanguage, const QString& categoryFilter)
@@ -84,9 +41,9 @@ void OpdsRequestManager::doUpdate(const QString& currentLanguage, const QString&
         query.addQueryItem("category", categoryFilter);
     }
 
-    auto mp_reply = opdsResponseFromPath("/catalog/v2/entries", query);
+    auto mp_reply = opdsResponseFromPath("/v2/entries", query);
     connect(mp_reply, &QNetworkReply::finished, this, [=]() {
-        handleReply(mp_reply, [this](QNetworkReply* r) { receiveContent(r); }, 0);
+        receiveContent(mp_reply);
     });
 }
 
@@ -101,22 +58,23 @@ QNetworkReply* OpdsRequestManager::opdsResponseFromPath(const QString &path, con
     url.setQuery(query);
     qInfo() << "Downloading" << url.toString(QUrl::FullyEncoded);
     QNetworkRequest request(url);
+    request.setAttribute(QNetworkRequest::RedirectPolicyAttribute, QNetworkRequest::NoLessSafeRedirectPolicy);
     return m_networkManager.get(request);
 }
 
 void OpdsRequestManager::getLanguagesFromOpds()
 {
-    auto mp_reply = opdsResponseFromPath("/catalog/v2/languages");
+    auto mp_reply = opdsResponseFromPath("/v2/languages");
     connect(mp_reply, &QNetworkReply::finished, this, [=]() {
-        handleReply(mp_reply, [this](QNetworkReply* r) { receiveLanguages(r); }, 0);
+        receiveLanguages(mp_reply);
     });
 }
 
 void OpdsRequestManager::getCategoriesFromOpds()
 {
-    auto mp_reply = opdsResponseFromPath("/catalog/v2/categories");
+    auto mp_reply = opdsResponseFromPath("/v2/categories");
     connect(mp_reply, &QNetworkReply::finished, this, [=]() {
-        handleReply(mp_reply, [this](QNetworkReply* r) { receiveCategories(r); }, 0);
+        receiveCategories(mp_reply);
     });
 }
 
